d125987131012800fd73c2225ad01f9ff4d2d642
deconf <chenjiagen88@gmail.com>
2025-12-25 23:56:06 +0800

    1.修正为可以编译过去的工具链 2. 同步修正工具链名称

--- arch/arm/cpu/aml_meson/config.mk
+++ arch/arm/cpu/aml_meson/config.mk
@@ -1,4 +1,4 @@
-CROSS_COMPILE=arm-none-eabi-
+CROSS_COMPILE=arm-eabi-
 ARM_CPU=cortex-a9
 PLATFORM_CPPFLAGS += $(call cc-option,-mcpu=cortex-a9 -mfpu=neon  -ffixed-r8 -mno-long-calls  -Wall -fPIC )
 #USE_PRIVATE_LIBGCC=yes
